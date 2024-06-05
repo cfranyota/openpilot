eca9133a901fd5555abeeba92e547aae177f7809
cfranyota <46506059+cfranyota@users.noreply.github.com>
2024-06-05 07:45:38 -0700

    frog or cydia tune

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -161,18 +161,19 @@ class CarInterface(CarInterfaceBase):
     sp_tss2_long_tune = Params().get_bool("ToyotaTSS2Long")
 
     tune = ret.longitudinalTuning
-    tune.deadzoneBP = [0., 9.]
-    tune.deadzoneV = [.0, .15]
+    #Frogpilot (swtich off) or Cydia tune (switch on)
+    tune.deadzoneBP = [0., 16., 20., 30.] if sp_tss2_long_tune else [0., 16., 20., 30.]
+    tune.deadzoneV =  [0., .03, .06, .15] if sp_tss2_long_tune else [.04, .05, .08, .15]
     if candidate in TSS2_CAR or ret.enableGasInterceptorDEPRECATED:
-      tune.kpBP = [0., 5., 20., 30.] if sp_tss2_long_tune else [0., 5., 20.]
-      tune.kpV = [1.3, 1.0, 0.7, 0.1] if sp_tss2_long_tune else [1.3, 1.0, 0.7]
-      tune.kiBP = [0.,   1.,    2.,    3.,   4.,   5.,    12.,  20.,  27., 40.] if sp_tss2_long_tune else [0., 5., 12., 20., 27.]
-      tune.kiV = [.348, .3361, .3168, .2831, .2571, .226, .198, .17,  .10, .01] if sp_tss2_long_tune else [.35, .23, .20, .17, .1]
+      tune.kpBP = [0., 5., 20.] if sp_tss2_long_tune else [0., 5.]
+      tune.kpV = [1.3, 1.0, 0.7] if sp_tss2_long_tune else [0.8, 1.]
+      tune.kiBP = [ 0.,  12.,  20., 27., 40.] if sp_tss2_long_tune else [0., 5.]
+      tune.kiV = [.35, .215, .195, .10, .01] if sp_tss2_long_tune else [.3, 1.]
       if candidate in TSS2_CAR:
-        ret.vEgoStopping = 0.15 if sp_tss2_long_tune else 0.25
-        ret.vEgoStarting = 0.15 if sp_tss2_long_tune else 0.25
-        ret.stopAccel = -0.4 if sp_tss2_long_tune else 0
-        ret.stoppingDecelRate = 0.05 if sp_tss2_long_tune else 0.3  # reach stopping target smoothly
+        ret.vEgoStopping = 0.15 if sp_tss2_long_tune else 0.15
+        ret.vEgoStarting = 0.15 if sp_tss2_long_tune else 0.15
+        ret.stopAccel = -2.5 if sp_tss2_long_tune else -2.5
+        ret.stoppingDecelRate = 0.17 if sp_tss2_long_tune else 0.17  # reach stopping target smoothly
     else:
       tune.kpBP = [0., 5., 35.]
       tune.kiBP = [0., 35.]
